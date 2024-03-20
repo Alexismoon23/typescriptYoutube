1a123ba925599f9bc748fbfd78bc11fc42497d0a
Alexismoon23 <128156806+Alexismoon23@users.noreply.github.com>
2024-03-20 09:37:42 +0100

    Changes

--- dataypes.ts
+++ dataypes.ts
@@ -1,1 +1,29 @@
-let lname = 'John';
+let lname : string;
+lname = "Alexis";
+let newname = lname.toUpperCase();
+console.log(newname);
+let age: number;
+age = 25;
+age = 25.5;
+let dob = "25";
+let result = parseInt(dob);
+let isValid: boolean=false;
+console.log(isValid);
+let empList: string[];
+empList = ["Alexis","Alexis1","Alexis2"];
+let numList ; Array<number>;
+numList = [1,2,2,3,4,5];
+let results = numList.filter((num)=> num> 2);
+let emp = empList.find((emp)=> emp === "Alexis");
+let sum = numList.reduce((acc,num)=> acc + num)
+console.log(results);
+console.log(emp);
+console.log(sum);
+
+enum Color {
+    Red,
+    Green,
+    Blue 
+}
+
+let c: Color = Color.Blue;
